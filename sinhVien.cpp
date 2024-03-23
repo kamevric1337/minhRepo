6efe535e2264c61f8868a1c776f0d25bc6bdb841
kamevric1337 <djswaggg69@gmail.com>
2024-03-23 21:34:16 +0700

    "Push again"

--- sinhVien.cpp
+++ sinhVien.cpp
@@ -34,5 +34,34 @@ int main()
     printf("Nhap CPA cho sv2: ");
     scanf("%f", &sv2.CPA);
 
+
+    // Dung con tro? de nhap thong tin cho mang cac sinh vien
+    SinhVien nhomSV[5];
+    SinhVien*p = nhomSV;
+
+    // chon so sinh vien muon nhap
+    int N;
+    printf("\nChon so sinh vien muon nhap thong tin(ko qua 10): ");
+    scanf("%d", &N);
+
+    for(int i = 0; i < N; i++)
+    {
+        printf("\nNhap thong tin cho sinh vien thu %d", i +1);
+        // Nhap mssv cho sinh vien
+        printf("\nNhap mssv cho sv2: ");
+        scanf("%d", &((p + i) -> mssv));
+        // Nhap ten cho sinh vien
+        printf("Nhap ten cho sv2: ");
+        fflush(stdin);
+        fgets((p + i) -> hoTen, 20, stdin);
+        // Nhap lop cho sinh vien
+        printf("Nhap lop cho sv2: ");
+        fgets((p + i)->lop, 10, stdin);
+        // Nhap GPA cho sinh vien
+        printf("Nhap CPA cho sv2: ");
+        scanf("%f", &((p + i ) -> CPA));
+    }
+
+
    
 }
